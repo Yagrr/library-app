ca237ae4c6a7b14fe44798aa47b3af378cb67704
Yagrr <90156764+Yagrr@users.noreply.github.com>
2024-05-25 01:47:36 +0100

    Add working addBook modal
    - Add book modal now appears when addBook button is pressed
    - Clicking on backdrop makes modal disappear
    - Modal styling is still unfinished

--- js/script.js
+++ js/script.js
@@ -5,14 +5,17 @@ const bkGrid = document.querySelector(".bkGrid");
 
 const btnAddBook = document.querySelector(".addBook");
 
-const modalNew = document.querySelector(".modalNew");
-const modalEdit = document.querySelector(".modalEdit");
+const backdropID = document.getElementById("backdropID");
+const modalNew = document.getElementById("modalNew");
+const modalEdit = document.getElementById("modalEdit");
 const modalDel = document.querySelector(".modalDelete");
 
 const library = [];
 
 // TODO: Add book card elements using bkCard.insertAdjacentHTML() method for a
 // given object
+
+// NOTE: Library interactions
 
 class Book {
     constructor(title, author, year, pages) {
@@ -29,27 +32,39 @@ function refreshLibrary() {
         bkGrid.insertAdjacentElement('beforeend',
             '<div class="bkCard>"'
                 + "<div class='bkInfo'>"
-                        + "<div class='bkTitle'>" + library[i].title + "</div>"
-                        + "<div class='bkAuthor'>" + library[i].author + "</div>"
-                        + "<div class='bkYear'>" + library[i].year + "</div>"
-                        + "<div class='bkPages'>" + library[i].pages + " pages</div>"
-                        + "<output></output>"
-                        + "<input class='bkPagesRead' type='range' min='0' max='" + library[i].pages + "' step='1' value='0' oninput='this.previousElementSibling.value = 'Pages read: ' + this.value'>"
-                        + "<div class='bkBtns'> <button class='editBk'>Edit</button> <button class='deleteBk'>X</button> </div>"
+                + "<div class='bkTitle'>" + library[i].title + "</div>"
+                + "<div class='bkAuthor'>" + library[i].author + "</div>"
+                + "<div class='bkYear'>" + library[i].year + "</div>"
+                + "<div class='bkPages'>" + library[i].pages + " pages</div>"
+                + "<output></output>"
+                + "<input class='bkPagesRead' type='range' min='0' max='" + library[i].pages + "' step='1' value='0' oninput='this.previousElementSibling.value = 'Pages read: ' + this.value'>"
+                + "<div class='bkBtns'> <button class='editBk'>Edit</button> <button class='deleteBk'>X</button> </div>"
                 + "</div>"
-            + "</div>"
+                + "</div>"
         );
     }
 }
 // NOTE: Button and modal interactions
 
 btnAddBook.addEventListener("click", () => {
-    modalNew.classList.toggle("show");
+    toggleModal(modalNew);
 });
 
-function toggleModal(e) {
-  const modal = document.querySelector(e);
-  const backdrop = document.querySelector('.backdrop');
-  modal.classList.toggle('show');
-  backdrop.classList.toggle('show');
+backdropID.addEventListener("click", () => {
+    closeModal(); 
+});
+
+function closeModal() {
+    const modal = document.querySelector(".modal");
+    const backdrop = document.querySelector(".backdrop");
+    modal.classList.remove("show");
+    backdrop.classList.remove("show");
 }
+
+function toggleModal(modal) {
+    const backdrop = document.querySelector(".backdrop");
+    backdrop.classList.toggle("show");
+    modal.classList.toggle("show");
+}
+
+
