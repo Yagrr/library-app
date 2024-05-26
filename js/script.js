e957cb74e04135e0c5cf516444a4cc9e5c121959
Yagrr <90156764+Yagrr@users.noreply.github.com>
2024-05-26 16:53:11 +0100

    Add pagesRead tracking via range input with HTML and JS book object integration
    Goal: Change card appearance if book is fully read
    Add feature: pagesRead is tracked using range input with different
    device support. Updates book object property pagesRead and HTML in
    accordance to input

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // Script for adding, removing book entries
 // and tracking pages
-// TODO: Add DOM manipulation for removing entry
 const bkGrid = document.querySelector(".bkGrid");
 
-const formNewBook = document.getElementById("formNewBook");
 const btnAddBook = document.querySelector(".addBook");
+
+const formNewBook = document.getElementById("formNewBook");
 const btnSubmit = document.querySelector(".bkSubmit");
 
 const backdropID = document.getElementById("backdropID");
@@ -15,30 +15,46 @@ const modalDel = document.querySelector(".modalDelete");
 // NOTE: Library interactions
 
 const library = [];
+
 class Book {
     constructor(title, author, year, pages) {
         this.title = title;
         this.author = author;
         this.year = year;
         this.pages = pages;
+        this.pagesRead = 0;
     }
 }
 
-// Add book card html elements onto grid
+addSampleBooks();
+
+function addSampleBooks() {
+    const title = "The C Programming Language 2nd ed.";
+    const author = "Brian Kernighan & Dennis Ritchie"; 
+    const year = "1988";
+    const pages = "238";
+    library.push( new Book(title, author, year, pages) );
+    refreshLibrary();
+}
+
 function refreshLibrary() {
-    // TODO: Fix bug with duplicated entries. Need to delete all entries first
-    delAllDisplayedBooks();
+    // Removes all displayed books first before redisplaying
+    while (bkGrid.firstChild) {
+        bkGrid.removeChild(bkGrid.lastChild);
+    }
+
     for (let i = 0; i < library.length; i++) {
         let bookHTML =
-            "<div class='bkCard'>"
-                    + "<div class='bkInfo' index = '" + i + "'>"
+
+            "<div class='bkCard' index='" + i + "'>"
+                    + "<div class='bkInfo'>"
                         + "<div class='bkTitle'>" + library[i].title + "</div>"
                         + "<div class='bkAuthor'>" + library[i].author + "</div>"
                         + "<div class='bkYear'>" + library[i].year + "</div>"
                         + "<div class='bkPages'>" + library[i].pages + " pages</div>"
                     + "</div>"
-                    + "<output>Pages read:</output>"
-                    + "<input class='bkPagesRead' type='range' min='0' max='" + library[i].pages + "' step='1' value='0' oninput='this.previousElementSibling.value = &#39;Pages read: &#39; + this.value' />"
+                    + "<output>Pages read: "+ library[i].pagesRead  +"</output>"
+                    + "<input class='bkPagesRead' type='range' min='0' max='" + library[i].pages + "' step='1' value='" + library[i].pagesRead + "' oninput='this.previousElementSibling.value = &#39;Pages read: &#39; + this.value' />"
                     +"<div class='bkBtns'>"
                         + "<button class='editBk'>Edit</button>" 
                         + "<button class='deleteBk'>X</button>"
@@ -47,7 +63,38 @@ function refreshLibrary() {
         bkGrid.insertAdjacentHTML('beforeend', bookHTML);
     }
 }
-// NOTE: Button and modal interactions
+
+// NOTE: Update pagesRead property when input range is used for a given card
+// HACK: Attaching event listeners to dynamic elements is annoying
+// Using event listener on grid. Trigger if target = bkPagesRead
+
+// Mouse
+bkGrid.addEventListener('mouseup', (e) => {
+    if ( e.target.classList.contains('bkPagesRead') ) {
+        updatePagesRead(e);
+    }
+});
+// Keyboard
+bkGrid.addEventListener('keyup', (e) => {
+    if ( e.target.classList.contains('bkPagesRead') ) {
+        updatePagesRead(e);
+    }
+});
+// Touchscreen
+bkGrid.addEventListener('ontouchend', (e) => {
+    if ( e.target.classList.contains('bkPagesRead') ) {
+        updatePagesRead(e);
+    }
+});
+
+function updatePagesRead(e) {
+        const inputPagesRead = e.target;
+        const card = inputPagesRead.parentNode;
+        const cardIndex = card.getAttribute("index");
+        library[cardIndex].pagesRead = inputPagesRead.value;
+}
+
+// NOTE: Book creation (modals and buttons)
 
 btnAddBook.addEventListener("click", () => {
     toggleModal(modalNew);
@@ -70,18 +117,6 @@ function createBook(event) {
     refreshLibrary();
 }
 
-// TODO: Write delete book function. Account for library array and html
-function delBook(event) {
-    event.preventDefault();
-
-}
-
-function delAllDisplayedBooks() {
-    while (bkGrid.firstChild) {
-        bkGrid.removeChild(bkGrid.lastChild);
-    }
-}
-
 backdropID.addEventListener("click", () => {
     closeModal(); 
 });
@@ -97,4 +132,13 @@ function toggleModal(modal) {
     const backdrop = document.querySelector(".backdrop");
     backdrop.classList.toggle("show");
     modal.classList.toggle("show");
+}
+
+// NOTE: Book deletion (buttons)
+
+// TODO: Write delete book function. 
+// Account for library array and html.
+function delBook(event) {
+    event.preventDefault();
+    
 }
